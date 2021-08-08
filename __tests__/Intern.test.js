87fc62a4b6e437b812e02ac2b25429e0e6edad49
jakeClark <jakemclark33@gmail.com>
2021-08-08 01:51:10 -0500

    Intern Test Messages

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,4 +1,28 @@
-const Intern = require('../lib/Intern');
+const EmployeeFixture = require('../fixtures/Employee.Fixture')
 
+describe('Intern', () => {
+  const intern = EmployeeFixture.intern()
 
-mock.jest('../lib/Intern');
+  it('should return correct name', () => {
+    expect(intern.getName()).toBe('Bob Dole')
+    expect(intern.name).toBe('Bob Dole')
+  })
+
+  it('should return correct id', () => {
+    expect(intern.getId()).toBe(1234)
+    expect(intern.id).toBe(1234)
+  })
+
+  it('should return correct email address', () => {
+    expect(intern.getEmail()).toBe('bob.dole@dingis.com')
+    expect(intern.email).toBe('bob.dole@dingis.com')
+  })
+  it('should return correct school', () => {
+    expect(intern.getSchool()).toBe('UW Madison')
+    expect(intern.school).toBe('UW Madison')
+  })
+
+  it('should return correct role', () => {
+    expect(intern.getRole()).toBe('Intern')
+  })
+})
--- fixtures/Employee.fixture.js
+++ fixtures/Employee.fixture.js
@@ -2,6 +2,7 @@
 const Employee = require('../lib/Employee')
 const Engineer = require('../lib/Engineer')
 const Manager = require('../lib/Manager')
+const Intern = require('../lib/Intern')
 class EmployeeFixture {
   static employee = () => {
     return new Employee('Bob Dole', 1234, 'bob.dole@dingis.com')
@@ -13,6 +14,10 @@ class EmployeeFixture {
 
   static manager = () => {
     return new Manager('Bob Dole', 1234, 'bob.dole@dingis.com', 123456789)
+  }
+
+  static intern = () => {
+    return new Intern('Bob Dole', 1234, 'bob.dole@dingis.com', 'UW Madison')
   }
 }
 module.exports = EmployeeFixture
